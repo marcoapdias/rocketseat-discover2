afede4d74c35483be5135f0a6010394584b6d43e
MarcoAPDias <marcoapdias@gmail.com>
2021-04-08 18:32:59 -0300

    :partial: Aula 2 - Dynamic Project Page, Refactoring Objects and Routes, POST Method for Profile and Job creation

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+// Template engine EJS
 server.set('view engine', 'ejs');
 
+// Arquivos statics
 server.use(express.static("public"));
+
+// User o req.body
+server.use(express.urlencoded());
 
 server.use(routes);
 
